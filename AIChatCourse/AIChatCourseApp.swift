a9ea798a1369d731200c6ee18a740710159f50b8
Mamricn <marcin.turek.27@wp.pl>
2026-02-09 17:06:03 +0000

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Marcin Turek on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
