8ba39084fbbcd293258d55cf2bc173436e5ce819
Mamricn <marcin.turek.27@wp.pl>
2026-02-15 18:24:08 +0000

    [Feature] CategoryCell

--- AIChatCourse/Core/Explore/ExploreView.swift
+++ AIChatCourse/Core/Explore/ExploreView.swift
@@ -9,16 +9,61 @@ import SwiftUI
 
 struct ExploreView: View {
     let avatar = AvatarModel.mock
+    @State private var featureAvatars: [AvatarModel] = AvatarModel.mocks
+    @State private var categories: [CharacterOption] = CharacterOption.allCases
     
     var body: some View {
         NavigationStack {
-            HeroCellView(
-                title: avatar.name,
-                subtitle: avatar.characterDescription,
-                imageName: avatar.profileImageName
-            )
-            .frame(height: 200)
+            List {
+                featuredSection
+                categorySection
+
+            }
                 .navigationTitle("Expplore")
+        }
+    }
+    
+    private var featuredSection: some View{
+        Section {
+            ZStack{
+                CarouselView(items: featureAvatars) { avatar in
+                    HeroCellView(
+                        title: avatar.name,
+                        subtitle: avatar.characterDescription,
+                        imageName: avatar.profileImageName
+                    )
+                }
+            }
+            .removeListRowFormating()
+            
+        } header: {
+            Text("Featured avatars")
+        }
+    }
+    
+    
+    
+    private var categorySection: some View{
+        Section {
+            ZStack{
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12){
+                        ForEach(categories, id: \.self) { category in
+                            CategoryCellView(
+                                title: category.rawValue.capitalized ,
+                                imageName: Constants.randomImage
+                            )
+                        }
+                    }
+                }
+                .frame(height:140)
+                .scrollTargetLayout()
+                .scrollTargetBehavior(.viewAligned)
+            }
+            .removeListRowFormating()
+            
+        } header: {
+            Text("Categories")
         }
     }
 }
