391fcdcd7f34ffb63f87369a1b8a7d5c73d50051
Mamricn <marcin.turek.27@wp.pl>
2026-03-18 23:02:15 +0000

    [Feature] App delegate methods

---
+++ AIChatCourse/Components/ViewModifires/AnyNotificationListenerViewModifire.swift
@@ -0,0 +1,36 @@
+//
+//  AnyNotificationListenerViewModifire.swift
+//  AIChatCourse
+//
+//  Created by Marcin Turek on 18/03/2026.
+//
+
+
+import Foundation
+import SwiftUI
+
+
+struct AnyNotificationListenerViewModifire: ViewModifier {
+    
+    
+    let notificationName: Notification.Name
+    let onNotificationRecipe: @MainActor (Notification) -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onReceive(NotificationCenter.default.publisher(for: notificationName ), perform: { notification in
+                onNotificationRecipe(notification)
+            })
+    }
+}
+extension View {
+    func onNotficationRecieved(name: Notification.Name, action: @MainActor @escaping(Notification) -> Void) -> some View {
+        modifier(AnyNotificationListenerViewModifire(notificationName: name, onNotificationRecipe: action))
+    }
+}
+
+//    .onReceive(Notification.default.publisher(for: UIApplication.willEnterForegroundNotification), perform: { notification in
+//        Task {
+//            await checkUserStatus()
+//        }
+//    })
--- AIChatCourse/Core/AppView/AppView.swift
+++ AIChatCourse/Core/AppView/AppView.swift
@@ -14,6 +14,7 @@ struct AppView: View {
     @Environment(AuthManager.self) private var authManager
     @Environment(UserManager.self) private var userManager
     @Environment(LogManager.self) private var logManager
+    @Environment(\.scenePhase) private var scenePhase
 
     
     @State var appState: AppState = AppState()
@@ -21,30 +22,48 @@ struct AppView: View {
     
     var body: some View {
         ZStack {
-            AppViewBuilder(
-                showTabBar: appState.showTabBar,
-                tabbarView: {
-                    TabBarView()
-                },
-                onboardingView: {
-                    WelcomeView()
-                }
-            )
-            .environment(appState)
-            .task{
-               await checkUserStatus()
-            }
-            .task{
-                try? await Task.sleep(for: .seconds(2))
-                await showATTPrompotIfNeeded()
-            }
-            .onChange(of: appState.showTabBar) { _, showToBar in
-                if !showToBar{
-                    Task{
+            
+            RootView(
+                delegate: RootDelegate(
+                    onApplicationDidAppear: nil,
+                    onApplicationWillEnterForeground: { _ in
+                        Task{
+                            await checkUserStatus()
+                        }
+                    },
+                    onApplicationDidBecomeActive: nil,
+                    onApplicationWillResignActive: nil,
+                    onApplicationDidEnterBackground: nil,
+                    onApplicationWillTerminate: nil
+                    
+                ),
+                content: {
+                    AppViewBuilder(
+                        showTabBar: appState.showTabBar,
+                        tabbarView: {
+                            TabBarView()
+                        },
+                        onboardingView: {
+                            WelcomeView()
+                        }
+                    )
+                    .environment(appState)
+                    .task{
                         await checkUserStatus()
                     }
+                    .task{
+                        try? await Task.sleep(for: .seconds(2))
+                        await showATTPrompotIfNeeded()
+                    }
+                    .onChange(of: appState.showTabBar) { _, showToBar in
+                        if !showToBar{
+                            Task{
+                                await checkUserStatus()
+                            }
+                        }
+                    }
                 }
-            }
+            )
         }
     }
     
